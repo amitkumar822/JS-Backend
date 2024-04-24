15f72a803c551a44ad5619df99b275369a9dd7a6
Amit kumar <140205046+amitkumar822@users.noreply.github.com>
2024-04-24 15:52:28 +0530

    all setup configured

--- 03BackendProject/src/app.js
+++ 03BackendProject/src/app.js
@@ -10,11 +10,19 @@ app.use(cors ({
 }))
 
 
-app.use(express.json({limit: '16kb'}))
+app.use(express.json({limit: "16kb"}))
 app.use(express.urlencoded({extended: true, limit: '16kb'}))
-app.use(express.static('public'))
+app.use(express.static("public"))
 app.use(cookieParser())
 
 
+// Routes import 
+import userRouter from './routes/user.routes.js'
+
+
+// Routes declaration
+app.use("/api/v1/users", userRouter)
+
+// http://localhost:8080/api/v1/users/register
 
 export { app }

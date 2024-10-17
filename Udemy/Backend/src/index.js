cd05790028fe86df750f58fddb185f08e5d3ba1a
Amit kumar <140205046+amitkumar822@users.noreply.github.com>
2024-10-17 12:12:01 +0530

    add wraper async error response and many more

--- Udemy/Backend/src/index.js
+++ Udemy/Backend/src/index.js
@@ -1,8 +1,18 @@
 import dotenv from "dotenv";
 import connectDB from "./db/index.js";
+import { app } from "./app.js";
 
 dotenv.config({
   path: "./.env",
 });
 
-connectDB();
+const PORT = process.env.PORT || 8001
+connectDB()
+.then(() => {
+  app.listen(PORT, () => {
+    console.log(`Server running at http://localhost:${PORT}`);
+  })
+})
+.catch((error) => {
+  console.log("MongoDB connection failed: " + error);
+})
